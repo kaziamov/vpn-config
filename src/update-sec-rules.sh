54ae8030e4542f8f4f8feee96c222e8763c1818a
Ilia Kaziamov <kaziamov@outlook.com>
2025-04-26 19:27:00 +0400

    add sec script

--- src/update-sec-rules.sh
+++ src/update-sec-rules.sh
@@ -1,6 +1,22 @@
-adduser vpnuser
-usermod -aG sudo vpnuser
+USER="$1"
+PORT="$2"
 
-su - vpnuser
 
-sudo nano /etc/ssh/sshd_config
+adduser $USER
+usermod -aG sudo $USER
+
+su - $USER
+
+
+echo "Now run script for creating SSH key on your machine
+https://github.com/kaziamov/vpn-config/tree/main/src/scripts
+for connect to VPN server"
+
+echo "
+PermitRootLogin no
+PasswordAuthentication no
+PubkeyAuthentication yes
+Port $PORT
+" >> /etc/ssh/sshd_config
+
+sudo systemctl restart sshd
