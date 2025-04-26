ccd5786dac204c13bf708481ab8db06e8ca44c18
Ilia Kaziamov <kaziamov@outlook.com>
2025-04-26 19:35:35 +0400

    add sec script

--- src/update-sec-rules.sh
+++ src/update-sec-rules.sh
@@ -1,6 +1,6 @@
-USER="$1"
-PORT="$2"
-
+PORT="$1"
+USER="$2"
+HOST="$3"
 
 adduser $USER
 usermod -aG sudo $USER
@@ -9,7 +9,9 @@ su - $USER
 
 
 echo "Now run script for creating SSH key on your machine
-https://github.com/kaziamov/vpn-config/tree/main/src/scripts
+
+sudo wget -O - https://raw.githubusercontent.com/kaziamov/vpn-config/main/src/scripts/generate-ssh.sh | sudo bash -s $1 $2 $3
+
 for connect to VPN server"
 
 echo "
